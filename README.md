5bda63cb33fdd5771ae65f31fd02c31214015616
James Newell <j.newell@nib.com.au>
2016-03-08 10:27:11 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# tradie-plugin-copy
+
+A plugin for copying random files.
+
+## Installation
+
+    npm --save-dev tradie-plugin-files
+
+## Usage
+
+Configure `.tradierc`:
+
+```json
+{
+  "plugins": [["files", {"files": ["*.html", "*.ico"], "src": "./src", "dest": "./dist"}]]
+}
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import debug from 'debug';
+import {watch, copy} from 'cpx';
+
+const dbg = debug('tradie-plugin-copy');
+
+/**
+ * A plugin that adds live-reloading to Tradie
+ * @param {tradie} tradie
+ * @param {object} config
+ */
+export default function(tradie, config) {
+  tradie.once('command.started', cmd => {
+
+    const watching = cmd.args.watch;
+    const src = config.src || tradie.config.scripts.src || tradie.config.styles.src;
+    const dest = config.dest || tradie.config.scripts.dest || tradie.config.styles.dest;
+    const patterns = [].concat(config.files || []);
+
+    //TODO: move running multiple files to a core part of cpx
+    const files = patterns.map(file => path.join(src, file)).join(',');
+
+    switch (cmd.name) {
+
+      case 'build':
+
+        if (watching) {
+
+          watch(files, dest, (err) => {
+            if (err) return tradie.emit('error', err);
+          })
+            .on('copy', e => dbg(`Copied file "${path.relative(dest, e.dstPath)}"`))
+          ;
+
+        } else {
+
+          copy(files, dest, (err) => {
+            if (err) return tradie.emit('error', err);
+          });
+
+        }
+
+        break;
+
+      default:
+        break;
+
+    }
+
+  });
+};
+
+
+//TODO tradie-plugin-serve for a real simple server, maybe redirect 404s to the index - superstatic?
